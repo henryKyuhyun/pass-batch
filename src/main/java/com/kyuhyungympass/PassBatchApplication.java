880d41d4b968970b47f2082e653107021fd58c62
HenryPark <selma457@gmail.com>
2023-07-20 21:16:10 +1000

    batch 기본설정

--- src/main/java/com/kyuhyungympass/PassBatchApplication.java
+++ src/main/java/com/kyuhyungympass/PassBatchApplication.java
@@ -1,13 +1,59 @@
 package com.kyuhyungympass;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+//EnableBatchProcessing 은 batch program 을 이용하기위한 기본
+@EnableBatchProcessing
 @SpringBootApplication
 public class PassBatchApplication {
+
+//	Job을 만들기위해 Step 을 만들고 그걸로 job을 만든다
+
+	private final JobBuilderFactory jobBuilderFactory;
+
+	private final StepBuilderFactory stepBuilderFactory;
+
+	public PassBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+		this.jobBuilderFactory = jobBuilderFactory;
+		this.stepBuilderFactory = stepBuilderFactory;
+	}
+
+	@Bean
+	public Step passStep(){
+		return this.stepBuilderFactory.get("passStep")
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+						System.out.println("Execute PassStep");
+						return RepeatStatus.FINISHED;
+					}
+				}).build();
+	}
+
+	@Bean
+	public Job passJob() {
+		return this.jobBuilderFactory.get("passJob")
+				.start(passStep())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
 	}
 
 }
+
+// https://github.com/kjs92980/pass-batch-starter	-> 기본프로젝트
+// https://github.com/kjs92980/pass-batch 		-> 전체내용 사용자 코드포함
